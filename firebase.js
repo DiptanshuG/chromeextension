733c788746cc06a4e3a747411c2b0d486f40f974
Diptanshu bhawsar <Diptanshubhawsar50@gmail.com>
2023-06-29 13:50:37 +0530

    update

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDyw5lBh0j_0lNMZy6eeRHleXeroWCJ3M4",
+    authDomain: "coupon-finder-a5cd6.firebaseapp.com",
+    projectId: "coupon-finder-a5cd6",
+    storageBucket: "coupon-finder-a5cd6.appspot.com",
+    messagingSenderId: "136273262261",
+    appId: "1:136273262261:web:143bf980710b52f02570fc",
+    measurementId: "G-HZ63B5GH6X"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
